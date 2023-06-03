73d79c64b06a39ccb1729d0e51c25a9eb31791c6
sachins010 <sachin.shirsath010@gmail.com>
2023-06-03 16:53:33 +0530

    initial Commit

---
+++ insert/insert.js
@@ -0,0 +1,37 @@
+//import modules
+const express = require('express')
+let mongodb = require('mongodb')
+//import url
+let url = require('../url')
+//create mongoclient
+let mcl = mongodb.MongoClient
+//create router instance
+let router = express.Router()
+//create rest api
+router.post("/", (req, res) => {
+    let obj = {
+        "s_id": req.body.s_id,
+        "s_name": req.body.s_name,
+        "percentage": req.body.percentage,
+        "Branch":req.body.Branch
+    }
+    //connect to mongodb
+    mcl.connect(url, (err, conn) => {
+        if (err)
+            console.log('Error in connection:- ', err)
+        else {
+            let db = conn.db('mongo')
+            db.collection('Students').insertOne(obj,(err)=>{
+                if(err)
+                    res.json({'insert':'Error '+err})
+                else{
+                    console.log('Data inserted')
+                    res.json({'insert':'Success'})
+                    conn.close()
+                }
+            })
+        }
+    })
+})
+//export router
+module.exports = router
